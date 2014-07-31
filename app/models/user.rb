6c4ccb87183da7da2b30374111e4920e89a6a87e
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2014-07-31 16:29:51 +0100

    google oauth2 sign in. create user after successful auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,14 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:google_oauth2]
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+    unless user
+      user = User.create(name: data["name"], first_name: data["first_name"], last_name: data["last_name"], email: data["email"], password: Devise.friendly_token[0,20])
+    end
+    user
+  end
 end
